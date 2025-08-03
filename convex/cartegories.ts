7e433c9654a7578e443070de33d49b9284661492
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-08-03 18:00:26 +0300

    fix

--- convex/cartegories.ts
+++
@@ -1,8 +0,0 @@
-import { query } from "./_generated/server";
-
-export const getCartegories = query({
-  args: {},
-  handler: async (ctx) => {
-    return await ctx.db.query("cartegories").collect();
-  },
-});
