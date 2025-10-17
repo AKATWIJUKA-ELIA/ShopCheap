55552967c8c5cf8b30129cd0556642777eb9a75d
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-10-17 08:20:10 +0300

    seller can edit shop details

--- convex/shops.ts
+++ convex/shops.ts
@@ -36,7 +36,21 @@ export const CreateShop = mutation({
         }
         })
 
-        
+         export const GetShopBySellerId = query({
+                args:{seller_Id:v.id("customers")},
+                handler:async(ctx,args)=>{
+                        const shop = await ctx.db.query("shops")
+                        .withIndex("by_owner_id", (q) => q.eq("owner_id", args.seller_Id))
+                        .unique();
+                        if (!shop) {
+                               return { success:false ,status: 404,message: "Shop not Found",user:null };
+                        }
+                        return { success:true, status: 200, message: "Shop found", shop: {
+                                ...shop
+                        } };
+                }
+                
+        })
  export const GetShopByName = query({
                 args:{shop_name:v.string()},
                 handler:async(ctx,args)=>{
@@ -44,13 +58,49 @@ export const CreateShop = mutation({
                         .withIndex("by_shop_name", (q) => q.eq("shop_name", args.shop_name))
                         .unique();
                         if (!shop) {
-                               return { success:false ,status: 404,message: "User not Found",user:null };
+                               return { success:false ,status: 404,message: "User not Found",shop:null };
                         }
+                        console.log("shop: ",shop);
                         return { success:true, status: 200, message: "User found", shop: {
                                 ...shop,
-                                profile_image: shop.profile_image ? await ctx.storage.getUrl(shop.profile_image) : "",
-                                cover_image:  shop.cover_image ? await ctx.storage.getUrl(shop.cover_image) : "",
                         } };
                 }
                 
         })
+export const UpdateShop = mutation({
+        args:{
+                shop: v.object({
+                        _id: v.id("shops"),
+                        shop_name: v.optional(v.string()),
+                        description: v.optional(v.string()),
+                        slogan: v.optional(v.string()),
+                        profile_image: v.optional(v.string()),
+                        cover_image: v.optional(v.string()),
+
+                })
+                },
+                        handler:async(ctx,args)=>{
+                                try{
+                                const shop = await ctx.db.get(args.shop._id);
+                                if(!shop){
+                                        return {success:false,message:"Shop not found",status:404};
+                                }
+                                console.log("id: ",args.shop.profile_image);
+                                const profile_image = args.shop.profile_image?.startsWith("https")? args.shop.profile_image: await ctx.storage.getUrl(args.shop.profile_image||"")
+                                console.log(profile_image);
+
+                                const cover_image = args.shop.cover_image?.startsWith("https")? args.shop.cover_image
+                                : await ctx.storage.getUrl(args.shop.cover_image||"");
+
+                                await ctx.db.patch(args.shop._id,{
+                                        shop_name: args.shop.shop_name,
+                                        description: args.shop.description,
+                                        slogan: args.shop.slogan,
+                                        profile_image:profile_image||"",
+                                        cover_image:cover_image||"",
+                                })
+                                return {success:true,message:"Shop updated successfully",status:200};
+                                }catch{
+                                        return {success:false,message:"Error updating shop",status:500};
+                                }
+                        }})
--- src/app/shops/[shopname]/page.tsx
+++ src/app/shops/[shopname]/page.tsx
@@ -2,19 +2,28 @@
 import React, { use } from "react";
 import useGetShopByName from "@/hooks/useGetShopByName";
 import Shop from "@/components/Shop/page";
-
+import { useEffect, useState } from "react";
 interface PageProps {
   params: Promise<{ shopname: string }>
 }
+import { ShopData } from "@/lib/types";
 
 const SellerShop = ({ params }: PageProps) => {
   const { shopname } = use(params);
   const { data: shop } = useGetShopByName(shopname);
+  console.log("shop data in page.tsx: ", shop?.shop);
+        const [shopData, setShopData] = useState<ShopData|null>(null);
+
+  useEffect(() => {
+        if(shop?.shop) {
+               setShopData(shop.shop); 
+        }
+  },[shop]);
 
 
   return (
     <div className="flex flex-col">
-      {shop?.shop && <Shop shop={shop.shop} />}
+      {shopData && <Shop shop={shopData} />}
     </div>
   );
 };
